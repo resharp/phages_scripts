dc66e1712e410d57d3f86d85b74aad5ce768ce72
Richard Gremmen <gremmen@resharp.nl>
2019-08-19 18:41:22 +0200

    adding some scripts to determine relations between genomes, contigs, phages and genes

--- run_blastp.sh
+++ run_blastp.sh
@@ -88,3 +88,60 @@ function run_mcl {
 
 }
 
+
+#-----------------------
+# TODO: move the following stuff, it has nothing to do with running blast, but processing the results from [virsorter -> prodigal]
+#-----------------------
+
+#this is messy stuff, we have to clean it up
+#purpose: getting the genome_id, contig_id, unique phage_name back from the generated individual protein names
+#this is so messy because it is the result of the [virsorter -> prodigal] pipeline part
+function translate_table {
+
+	#cut off the category and put ; in front of genome_id
+	#cat annotations/IP_translation.txt | sed -e 's/-cat_[0-9]*_[0-9]*/;\0/' | sed -e 's/[0-9]*_[0-9]*_;/;\0/'
+
+	#this one is for the prophages (ontaining genes)
+	#cat annotations/IP_translation.txt | sed -e 's/-cat_[0-9]*_[0-9]*/;\0/' | sed -e 's/_gene_[0-9]*_gene_[0-9]*-[0-9]*-[0-9]*;/;\0/g'
+
+	#now do everything at the same time
+	#we put in some ";" delimiters in intermediary steps to help selecting parts later on
+	cat annotations/IP_translation.txt | sed -e 's/-cat_[0-9]*_[0-9]*/;\0/' | sed -e 's/[0-9]*_[0-9]*_;/;\0/' |\
+		sed -e 's/_gene_[0-9]*_gene_[0-9]*-[0-9]*-[0-9]*;/;\0/g' |\
+		sed -e 's/___\([0-9]*_[0-9]*_\);/;\1/' |\
+		sed -e 's/___\([0-9]*_[0-9]*_-circular\);/;\1/' |\
+		sed -s 's/circular-cat/circular;-cat/g'
+
+}
+
+function genomes_from_proteins {
+	translate_table | cut -d ';' -f2 | sed -e 's/_-circular$//g' | sed -e 's/__/_;_/g' | cut -d ';' -f1 | sed -e 's/_$//g'
+}
+
+function gene_part_from_proteins {
+	translate_table | cut -d ';' -f2 | sed -e 's/\(gene_[0-9]*_gene_[0-9]*\)/;\1;/g' |\
+		 sed -e 's/^[0-9]*_[0-9]*_//g' | sed -e 's/_;//' | cut -d ';' -f1
+}
+
+
+function contig_names_from_proteins {
+	translate_table | cut -d ';' -f1 | cut -f2 | sed -e 's/VIRSorter_//g'
+}
+
+#we have the separate parts that make a phage unique and can optionally concatenate them again with _ (not strictly necessary)
+function show_phage_names {
+	paste <(genomes_from_proteins) <(contig_names_from_proteins) <(gene_part_from_proteins) | awk '{print $1"_"$2"_"$3}'
+}
+
+function show_phages_with_most_proteins {
+	show_phage_names | sort | uniq -c | sort -k1 -n -r
+}
+
+function show_genomes_with_most_phages {
+	paste <(genomes_from_proteins) <(show_phage_names) | sort | uniq | cut -f1 | sort | uniq -c | sort -k1 -n -r
+}
+
+function show_phages_for_genome {
+	genome=$1
+	show_phage_names | grep $genome | sort | uniq
+}
