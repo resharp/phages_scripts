7d5800d0058298eb4979e5cfeaaa06f0f89fbea1
Richard Gremmen <gremmen@resharp.nl>
2019-08-20 14:35:02 +0200

    added script to run prodigal on 1,2,4,5 phages from all genomes

--- run_prodigal.sh
+++ run_prodigal.sh
@@ -85,7 +85,7 @@ function run_prodigal_on_some_samples {
 }
 
 
-function run_prodigal_on_all_samples {
+function run_prodigal_on_all_samples_old {
 
 	mutant=$1
 	samples=$(get_phage_samples_for_cat_1_2_4_5 $mutant)
@@ -111,4 +111,62 @@ function calculate_average_gc_content {
 	echo "Average GC content" 
 	cat annotations/all.faa | grep ">" | cut -d ";" -f6 | cut -d "=" -f2 | awk '{ total += $1; count++ } END { print total/count }'
 
+}
+
+
+function run_prodigal_on_all_samples {
+
+
+	#TODO: select all not just the first 100
+	#---
+	genomes=$(ll /hosts/linuxhome/mgx/DB/PATRIC/patric/phages | awk '{print $9}' | grep -E '[0-9]+\.[0-9]+' | head -100 )
+
+
+	tempfile=all_12_45.fasta
+
+	for genome in $genomes
+	do
+
+	        date_start=$(date)
+                log_line="${date_start} Running prodigal for genome ${genome}"
+
+                echo $log_line >> bin/run_prodigal.txt
+
+		genome_dir=/hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes/$genome
+
+		mkdir $genome_dir
+
+		phage_files=$(find /hosts/linuxhome/mgx/DB/PATRIC/patric/phages/$genome/VIRSorter_*.fasta | grep -e 'cat-[1-2|4-5]') 
+
+		if [ ! -z "$phage_files" ]
+		then
+
+			#empty input fasta file for prodigal (in order to repeat runs without side effects)
+			> $genome_dir/$tempfile
+
+			for phage_file in $phage_files
+			do
+				#echo $phage_file
+				cat $phage_file >> $genome_dir/$tempfile
+			done
+
+			#echo "running prodigal on " $genome
+
+			prodigal -i $genome_dir/$tempfile\
+				-o $genome_dir/$genome.genes -a $genome_dir/$genome.proteins.faa -p meta -q
+
+			#todo: remove tempfile
+			# rm -f $genome_dir/$tempfile
+
+		fi
+	done
+
+}
+
+function cleanup_prodigal {
+
+	#remove all gene predictions from prodigal
+	rm -f -r /hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes
+
+	mkdir /hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes
 }
