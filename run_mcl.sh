0461f0865f8780f19aba3d4b5167565a0bf28a7c
Richard Gremmen <gremmen@resharp.nl>
2019-08-23 12:39:29 +0200

    adapted run_mcl scripts to run clustering on filtered pairwise blastp results with 75% coverage threshold

--- run_mcl.sh
+++ run_mcl.sh
@@ -21,6 +21,19 @@ function prepare_abc_for_mcl {
 
 }
 
+function prepare_abc_for_mcl75 {
+
+
+	#TODO: change location, this one is only running on 1000 genomes
+	#---- * *  *     *               *
+	gene_dir=/hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
+
+	#prepare format for mcl (without weight)
+	cat $gene_dir/pairwise_blastout_filtered_75coverage.txt | awk '{print $1 "\t" $2}' > $gene_dir/pw_blastout_mcl_75.abc
+
+}
+
+#mcl is installed in conda env virsorter
 function run_mcl {
 
 	#default inflation is 2.0. we run with 2.5 for comparing with cytoscape plug-in
@@ -30,51 +43,61 @@ function run_mcl {
 	#---- * *  *     *               *
 	gene_dir=/hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
 
+	file_in_mcl=pw_blastout_mcl_75.abc
 	#run with different inflations
 
 	samples="1.5 2.0 2.5 3.0"
 	for sample in $samples:
 	do
-		mcl $gene_dir/pw_blastout_mcl.abc --abc -I ${sample} -te 4 --d
+		mcl $gene_dir/$file_in_mcl --abc -I ${sample} -te 4 --d
 	done
 
 	samples="I15 I20 I25 I30"
 
 	for sample in $samples
 		do echo "largest clusters for inflation: "$sample
-		cat $gene_dir/out.pw_blastout_mcl.abc.$sample | awk '{print NF" proteins"}' | head -3
+		cat $gene_dir/out.$file_in_mcl.$sample | awk '{print NF" proteins"}' | head -3
 	done
 
 }
 
+#example split_mcl I25
 function split_mcl {
 
 	#samples denote runs for different mcl inflation factors
-	sample="I20"
+	sample=$1
 
 	gene_dir=/hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
 
-	mkdir $gene_dir/mcl.$sample
+	##NB processes 75 coverage file
+	# * *  *   *    *
+	file_out_mcl=out.pw_blastout_mcl_75.abc
+
+	#mkdir $gene_dir/mcl.$sample
+	mkdir $gene_dir/mcl_75.$sample
 
 	n=1
-	cat $gene_dir/out.pw_blastout_mcl.abc.$sample |\
+	cat $gene_dir/$file_out_mcl.$sample |\
 		while read line
 		do
 			for word in $line
 			do
-				echo $word >> $gene_dir/mcl.$sample/PC_$n.txt
+				echo $word >> $gene_dir/mcl_75.$sample/PC_$n.txt
 			done
 			n=$((n+1))
 		done
 }
 
+#uses seqtk in python37 conda env
+#example  split_fasta_according_to_mcl I25
 function split_fasta_according_to_mcl {
 
 	gene_dir=/hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
 
-	sample="I20"
+	sample=$1
 
-	files=$(find $gene_dir/mcl.$sample/PC_*.txt)
+	files=$(find $gene_dir/mcl_75.$sample/PC_*.txt)
+
 	for file in $files
 	do
 		#run your tool here
