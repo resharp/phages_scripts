acf39162209702fa0265ecd8d3828a2d2014d9ea
Richard Gremmen <gremmen@resharp.nl>
2019-08-29 11:00:35 +0200

    some fixed and parametrizing on gene_dir

--- view_stats.sh
+++ view_stats.sh
@@ -3,7 +3,10 @@
 #
 # processing the results from [virsorter -> prodigal]
 #
+# building tables for coupling of phage to IP (this could be moved to end of run_prodigal script)
+#
 # also translating gene content into protein clusters of a specific clustering
+#	(this could be moved to end of run_mcl script)
 #-----------------------
 
 #this is messy stuff, we have to clean it up
@@ -11,7 +14,7 @@
 #this is so messy because it is the result of the [virsorter -> prodigal] pipeline part
 function translate_table {
 
-	gene_dir=/hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
+	gene_dir=$1
 
 	#cut off the category and put ; in front of genome_id
 	#cat $gene_dir/IP_translation.txt | sed -e 's/-cat_[0-9]*_[0-9]*/;\0/' | sed -e 's/[0-9]*_[0-9]*_;/;\0/'
@@ -30,21 +33,26 @@ function translate_table {
 }
 
 function genomes_from_proteins {
-	translate_table | cut -d ';' -f2 | sed -e 's/_-circular$//g' | sed -e 's/__/_;_/g' | cut -d ';' -f1 | sed -e 's/_$//g'
+	gene_dir=$1
+	translate_table $gene_dir | cut -d ';' -f2 | sed -e 's/_-circular$//g' | sed -e 's/__/_;_/g' | cut -d ';' -f1 | sed -e 's/_$//g'
 }
 
 function gene_part_from_proteins {
-	translate_table | cut -d ';' -f2 | sed -e 's/\(gene_[0-9]*_gene_[0-9]*\)/;\1;/g' |\
+	gene_dir=$1
+	translate_table $gene_dir | cut -d ';' -f2 | sed -e 's/\(gene_[0-9]*_gene_[0-9]*\)/;\1;/g' |\
 		 sed -e 's/^[0-9]*_[0-9]*_//g' | sed -e 's/_;//' | cut -d ';' -f1
 }
 
 function contig_names_from_proteins {
-	translate_table | cut -d ';' -f1 | cut -f2 | sed -e 's/VIRSorter_//g'
+	gene_dir=$1
+	translate_table $gene_dir | cut -d ';' -f1 | cut -f2 | sed -e 's/VIRSorter_//g'
 }
 
-function show_phage_names {
+#example:
+# show_phage_names /hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
+function show_phage_names_and_ips {
 
-	gene_dir=/hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
+	gene_dir=$1
 
 	#we have the separate parts that make a phage unique and can optionally concatenate them again with _ (not strictly necessary)
 	#paste <(genomes_from_proteins) <(contig_names_from_proteins) <(gene_part_from_proteins) | awk '{print $1"_"$2"_"$3}'
@@ -52,11 +60,22 @@ function show_phage_names {
 	cat $gene_dir/IP_translation.txt | sed -e 's/_[0-9]*$/;\0/g' | cut -d ';' -f1 | cut -f1,2
 }
 
+function show_phage_names {
+
+	gene_dir=$1
+
+	#we have the separate parts that make a phage unique and can optionally concatenate them again with _ (not strictly necessary)
+	#paste <(genomes_from_proteins) <(contig_names_from_proteins) <(gene_part_from_proteins) | awk '{print $1"_"$2"_"$3}'
+
+	cat $gene_dir/IP_translation.txt | sed -e 's/_[0-9]*$/;\0/g' | cut -d ';' -f1 | cut -f2
+}
+
 
 # format: PH_Id, PH_Name
 # from prodigal gene predictions (not specific to any clustering)
 function show_phage_table {
-	gene_dir=/hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
+
+	gene_dir=$1
 
 	#we have the separate parts that make a phage unique and can optionally concatenate them again with _ (not strictly necessary)
 	#paste <(genomes_from_proteins) <(contig_names_from_proteins) <(gene_part_from_proteins) | awk '{print $1"_"$2"_"$3}'
@@ -70,10 +89,10 @@ function show_phage_table {
 # format: PH_Id, IP_Id, PH_Name
 function make_phage_ip_table {
 
-	gene_dir=/hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
+	gene_dir=$1
 
 	#join on phage_name (show_phage_table already sorted on phage name)
-	join -1 2 -2 2 -o2.1,1.1,0 <(cat $gene_dir/IP_translation.txt | sed -e 's/_[0-9]*$/;\0/g' | cut -d ';' -f1 | sort -k2) <(show_phage_table)\
+	join -1 2 -2 2 -o2.1,1.1,0 <(cat $gene_dir/IP_translation.txt | sed -e 's/_[0-9]*$/;\0/g' | cut -d ';' -f1 | sort -k2) <(show_phage_table $gene_dir)\
 		 > $gene_dir/phage_ip_table.txt
 }
 
@@ -85,7 +104,7 @@ function make_ip_pc_table {
 
 	sample=$1
 
-	gene_dir=/hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
+	gene_dir=$2
 
 	pc_file=out.pw_blastout_mcl_75.abc.$sample
 
@@ -127,29 +146,37 @@ function make_ip_pc_table {
 
 
 function show_phages_with_most_proteins {
-	show_phage_names | sort | uniq -c | sort -k1 -n -r
+	gene_dir=$1
+
+	cat $gene_dir/phage_ip_table.txt | tr " " "|" | cut -d '|' -f1 | uniq -c | sort -k1 -n -r
+
+	#show_phage_names_and_ips $gene_dir | sort | uniq -c | sort -k1 -n -r
 }
 
 function show_genomes_with_most_phages {
-	paste <(genomes_from_proteins) <(show_phage_names) | sort | uniq | cut -f1 | sort | uniq -c | sort -k1 -n -r
+	gene_dir=$1
+	paste <(genomes_from_proteins $gene_dir ) <(show_phage_names $gene_dir) | sort | uniq | cut -f1 | sort | uniq -c | sort -k1 -n -r
 }
 
 function show_phages_for_genome {
 	genome=$1
-	show_phage_names | grep $genome | sort | uniq
+	gene_dir=$1
+	show_phage_names $gene_dir | grep $genome | sort | uniq
 }
 
 
+#example
+# stats_phages_1245 /hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
 function stats_phages_1245 {
 
-	gene_dir=/hosts/linuxhome/mgx/DB/PATRIC/patric/phage_genes_1000
+	gene_dir=$1
 
 	echo " --- "
 	echo "Nr of phages of categories 1,2,4,5 respectively:"
-	show_phage_names | sort | uniq | grep -c cat_1
-	show_phage_names | sort | uniq | grep -c cat_2
-	show_phage_names | sort | uniq | grep -c cat_4
-	show_phage_names | sort | uniq | grep -c cat_5
+	show_phage_names $gene_dir | sort | uniq | grep -c cat_1
+	show_phage_names $gene_dir | sort | uniq | grep -c cat_2
+	show_phage_names $gene_dir | sort | uniq | grep -c cat_4
+	show_phage_names $gene_dir | sort | uniq | grep -c cat_5
 	echo " --- "
 	echo "Nr of genes from phages of categories 1,2,4,5 respectively:"
 	grep -c cat_1 $gene_dir/IP_translation.txt
