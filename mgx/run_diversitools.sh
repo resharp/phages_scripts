07c68cf4c309f54dc45b244c65fba1af0de01a4d
Richard Gremmen <gremmen@resharp.nl>
2019-12-09 12:24:21 +0100

    run diversitools and CalcDiversiMeasures on a batch of bam files

--- mgx/run_diversitools.sh
+++ mgx/run_diversitools.sh
@@ -1,30 +1,87 @@
-#DiversiTools
-#http://josephhughes.github.io/DiversiTools/tutorial.html
-
+#
+# DiversiTools
+# http://josephhughes.github.io/DiversiTools/tutorial.html
+#
+# CalcDiversiMeasures.py
+# https://github.com/resharp/phages
+#
+#
 
 #sample=MGXDB008660
-sample=MGXDB009139
-#sample_dir=/hosts/linuxhome/mutant31/tmp/richard/crassphage_samples/${sample}
+
+#TODO: change to other mutant
+#------ ***
+sample_dir=/hosts/linuxhome/mutant14/tmp/richard/crassphage_samples
+
+master_dir=/hosts/linuxhome/chaperone/tmp/richard/crassphage_samples
+admin_dir=/hosts/linuxhome/mgx/DB/MGXDB
+
+function run_gene_samples {
+
+	# run 20 gene samples
+	#TODO: change to the number of samples you would like to use
+	samples=$(cat $admin_dir/taxon_1211417_counts_sorted.txt | awk '{if ($3>10000) print $1}' | head -20 | grep -v "sample")
+
+	#samples=$(echo "MGXDB000864")
+
+	for sample in $samples
+	do
+		echo $sample
+
+		#copy bam file to $sample_dir
+
+		mkdir $sample_dir/${sample}
+		/bin/cp $master_dir/${sample}/${sample}_filtered.sorted.bam $sample_dir/${sample}/${sample}_filtered.sorted.bam
+		/bin/cp $master_dir/${sample}/${sample}_filtered.sorted.bam.bai $sample_dir/${sample}/${sample}_filtered.sorted.bam.bai
+		#ll $sample_dir/${sample}
+
+		echo "start run diversiutils"
+		run_diversiutils $sample_dir $sample
+		#ll $sample_dir/${sample}
+
+		run_calc_measures $sample_dir $sample
+
+		#copy results back to right directory in master dir
+		/bin/cp $sample_dir/${sample}/${sample}_*.txt $master_dir/${sample}
+
+
+		#clean up (not all)
+		rm -f $sample_dir/${sample}/${sample}_filtered.sorted.bam
+		rm -f $sample_dir/${sample}/${sample}_filtered.sorted.bam.bai
+		rm -f $sample_dir/${sample}/${sample}_AA.txt
+		rm -f $sample_dir/${sample}/${sample}_entropy.txt
+		rm -f $sample_dir/${sample}/${sample}_read.txt
+
+	done
+}
+
 
 function run_diversiutils {
 
-	sample=$1
+	sample_dir=$1
+	sample=$2
 
 	tools/DiversiTools/bin/diversiutils_linux -bam $sample_dir/${sample}/${sample}_filtered.sorted.bam\
 		-ref $sample_dir/crassphage_refseq.fasta\
-		-orfs $sample_dir/codingregions_new.txt\
-		-stub $sample_dir/${sample}
+		-orfs scripts/mgx/crassphage_codingregions.txt\
+		-stub $sample_dir/${sample}/${sample}
 
 	#remove the "<NA>" strings
 	sed -e 's/<NA>//g' $sample_dir/${sample}/${sample}_AA.txt > $sample_dir/${sample}/${sample}_AA_clean.txt
+}
+
+function run_calc_measures {
+
+	sample_dir=$1
+	sample=$2
 
 	conda deactivate
 	conda activate python37
 
 	#create measures (based on ${sample}_AA_clean.txt)
-	python bin/CalcDiversiMeasures.py
-
+	python source/phages/CalcDiversiMeasures.py -d $sample_dir -s $sample
 }
+
 
 #cut -f3,5,11,12,13 $sample_dir/MGXDB008660_CDS_AA.txt | less
 
@@ -49,13 +106,5 @@ function convert_aa_file {
 	#remove the "<NA>" strings
 	sed -e 's/<NA>//g' $sample_dir/${sample}/${sample}_AA.txt > $sample_dir/${sample}/${sample}_AA_clean.txt
 
-}
-
-
-gene=KP06_gp66
-#view_gene_metrics $gene
-function view_gene_metrics {
-	gene=$1
-	# | less
 }
 
