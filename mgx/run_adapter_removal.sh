9845726899ecf6f50a0df61cac00c320764708b6
Richard Gremmen <gremmen@resharp.nl>
2020-01-29 16:27:23 +0100

    run adapter removal on multiple runs of a project

--- mgx/run_adapter_removal.sh
+++ mgx/run_adapter_removal.sh
@@ -11,12 +11,16 @@ function run_adapter_removal_on_project {
 	# ERR525689
 
 	# to do: remove head -1 if you want to run all files
-	files_1=$(find $project_dir/*_1.fastq.gz | head -1)
+	files_1=$(find $project_dir/*_1.fastq.gz | head -2)
 
 	for file_1 in $files_1
 	do
-		file_2=$(echo $file_1 | sed -e 's/_1/_2/g')
-		run_adapter_removal $project_dir $file_1 $file_2
+		base_file_1=$(basename "$file_1")
+		# e.g. run=ERR525689
+
+		run=${base_file_1%_1.fastq.gz}
+
+		run_adapter_removal $project_dir $run
 	done
 
 }
@@ -24,16 +28,18 @@ function run_adapter_removal_on_project {
 function run_adapter_removal {
 
 	project_dir=$1
-	file_1=$2
-	file_2=$3
+	run=$2
 
-	echo "Starting to run AdapterRemoval"
-	echo $file_1
-	echo $file_2
-	base=ERR525689.output_paired
+	file_1=${project_dir}/${run}_1.fastq.gz
+	file_2=${project_dir}/${ryn}_2.fastq.gz
+	basename=${project_dir}/${run}
 
-	# to do use --gzip option for smaller files
-	# --gzip
-	AdapterRemoval --file1 $file_1 --file2 $file_2 --basename $project_dir/$base --trimns --trimqualities --collapse --threads 2 --minquality 25 --gzip
+	echo "Starting to run AdapterRemoval for "$run" in "$project_dir
+	#echo $file_1
+	#echo $file_2
+	#echo $basename
+	# e.g. basename=${project_dir}/ERR525689
+
+	AdapterRemoval --file1 $file_1 --file2 $file_2 --basename $basename --trimns --trimqualities --collapse --threads 2 --minquality 25 --gzip
 
 }
