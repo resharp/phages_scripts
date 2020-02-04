460c9a6ee059867e8689a2076b4dafa46b8c560a
Richard Gremmen <gremmen@resharp.nl>
2020-02-04 18:37:11 +0100

    minor changes

--- mgx/Guerin_to_ref.sh
+++ mgx/Guerin_to_ref.sh
@@ -1,7 +1,7 @@
 
 # first cd to
 # /hosts/linuxhome/chaperone/tmp/richard/guerin_data
-sample_dir=/hosts/linuxhome/chaperone/tmp/richard/guerin_data
+# sample_dir=/hosts/linuxhome/chaperone/tmp/richard/guerin_data
 # cd $sample_dir
 
 function split_fasta_by_ids {
@@ -15,7 +15,9 @@ function split_fasta_by_ids {
 	# for sample in $samples; do echo $sample; done
 	for sample in $samples
 		do
-			grep -A1 -w $sample Guerin_subset.fa > ${sample}.fasta
+			rm -f -r ${sample}
+			mkdir ${sample}
+			grep -A1 -w $sample Guerin_subset.fa > ${sample}/${sample}.fasta
 		done
 }
 
@@ -27,30 +29,16 @@ function run_prodigal_on_ref {
 	conda activate prodigal
 
 	# prodigal -i $fasta_file -o annotations/my.genes -a annotations/my.proteins.faa -p meta
-	files=$(find *.fasta)
+	files=$(find */*.fasta)
 	for file in $files
 	do
-		sample=$(echo $file | cut -f1 -d '.')
+		name=$(basename $file)
+		sample=$(echo $name | cut -f1 -d '.')
 
-		mkdir ${sample}
-
-		# here we should run it on a single genomen (does it matter?)
-		prodigal -i $file -o ${sample}/${sample}.genes -a ${sample}/${sample}.proteins.faa -p single
 		# echo $sample
+		# here we should run it on a single genomen (does it matter?)
+		prodigal -i $file -o ${sample}/${sample}.genes -a ${sample}/${sample}.proteins.faa -p meta
 
 	done
 	# prodigal -i $file -o annotations/my.genes -a annotations/my.proteins.faa -p meta
-}
-
-
-function remove_subdirs {
-
-	files=$(find *.fasta)
-	for file in $files
-	do
-		sample=$(echo $file | cut -f1 -d '.')
-
-		rm -f -r  ${sample}
-	done
-
 }
