d90baa90938d9f831d37cbb211d5c794a580ad33
Richard Gremmen <gremmen@resharp.nl>
2020-02-18 13:45:03 +0100

    simple sample stats for further analysis in Python

---
+++ mgx/sample_stats.sh
@@ -0,0 +1,24 @@
+function sample_stats {
+
+	sample_dir=$1
+
+	if [ -z "$sample_dir" ]
+	then
+		echo "please supply sample_dir as argument"
+	else
+		echo "writing sample stats to " ${sample_dir}/sample_stats.txt
+		paste <(find $sample_dir/*/*sorted.idstats.txt |\
+	                while read LINE
+	                do
+	                        base=$(basename "$LINE")
+	                        sample=${base%.sorted.idstats.txt}
+	                       echo $sample; \
+        		done) \
+		<(cat $sample_dir/*/*sorted.idstats.txt | awk 'NR%2==1 {print $1}') \
+		<(cat $sample_dir/*/*sorted.idstats.txt | awk 'NR%2==1 {print $3}') \
+		<(cat $sample_dir/*/*sorted.idstats.txt | awk 'NR%2==0 {print $4}') > $sample_dir/sample_stats.txt
+	fi
+}
+
+
+
